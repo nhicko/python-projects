af3cf912ff96311053e3cc15bbc90373bb080735
nhicko <npmcatipon@gmail.com>
2023-10-26 21:19:50 +0800

    half completed hangman

---
+++ hangman.py
@@ -0,0 +1,44 @@
+import random
+from words import words
+import string
+
+
+def get_valid_word(words):
+    word = random.choice(words)
+    while '-' in word or ' ' in word:
+        word = random.choice(words)
+    return word.upper()
+
+
+def hangman():
+    word = get_valid_word(words)
+    word_letters = set(word)
+    alphabet = set(string.ascii_uppercase)
+    used_letters = set()  # what the user has guessed
+
+    # getting user input
+    while len(word_letters) > 0:
+        # letters used
+        #  ' '.join(['a', 'b', 'cd']) -> 'a b cd'
+        print('You have used these letters: ', ' '.join(used_letters))
+
+        # what current word is (ie W - R D)
+        word_list = [letter if letter in used_letters else '-' for letter in word]
+        print('Current word: ', ' '.join(word_list))
+
+        # Guess the letter
+        user_letter = input('Guess a letter: ').upper()
+        if user_letter in alphabet - used_letters:
+            used_letters.add(user_letter)
+            if user_letter in word_letters:
+                word_letters.remove(user_letter)
+
+        elif user_letter in used_letters:
+            print('You have already used that character. please try again.')
+
+        else:
+            print('Invalid character. please try again')
+    print(f'Congratiolations, you guess: {word}')
+
+
+hangman()
